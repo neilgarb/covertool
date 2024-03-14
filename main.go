d2775b3212177be0b874e30890285d0b7a65e192
Neil Garb <neil@luno.com>
2024-03-14 11:55:26 +0200

    Add -grouping

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sort"
 	"strings"
 
 	"golang.org/x/tools/cover"
@@ -16,9 +18,11 @@ import (
 func main() {
 	var fileName, baseDir string
 	var trace bool
+	var grouping string
 	flag.StringVar(&fileName, "profile", "", "Coverage profile produced by `go test -coverprofile`")
 	flag.StringVar(&baseDir, "base", ".", "Where to find the files in the coverage profile")
 	flag.BoolVar(&trace, "trace", false, "Output debug info")
+	flag.StringVar(&grouping, "grouping", "", "If provided, results will be matched against this regex, and grouped by the 1st grouped match, e.g. 'foo/([^/]*)/' will group foo/bar/baz as bar")
 	flag.Parse()
 
 	traceFn := func(string) {}
@@ -28,18 +32,47 @@ func main() {
 		}
 	}
 
-	if err := run(fileName, baseDir, traceFn); err != nil {
+	var groupRe *regexp.Regexp
+	if grouping != "" {
+		r, err := regexp.Compile(grouping)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		groupRe = r
+	}
+
+	if err := run(fileName, baseDir, traceFn, groupRe); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func run(fileName, baseDir string, traceFn func(string)) error {
+type bucket struct {
+	statements int
+	count      int
+}
+
+func (b bucket) add(bl cover.ProfileBlock) bucket {
+	b.statements += bl.NumStmt
+	b.count += bl.Count
+	return b
+}
+
+func (b bucket) coverPct() string {
+	if b.statements == 0 {
+		return "0.00%"
+	}
+	return fmt.Sprintf("%.2f%%", float64(b.count)/float64(b.statements)*100)
+}
+
+func run(fileName, baseDir string, traceFn func(string), groupRe *regexp.Regexp) error {
 	profiles, err := cover.ParseProfiles(fileName)
 	if err != nil {
 		return err
 	}
-	var totStmt, totCount int
+	var total bucket
+	buckets := make(map[string]bucket)
 	for _, profile := range profiles {
 		if gen, err := isGenerated(filepath.Join(baseDir, profile.FileName)); err != nil {
 			return err
@@ -48,18 +81,31 @@ func run(fileName, baseDir string, traceFn func(string)) error {
 			continue
 		}
 		for _, block := range profile.Blocks {
-			totStmt += block.NumStmt
-			totCount += block.Count
+			total = total.add(block)
+			if groupRe != nil {
+				bucketName := "other"
+				m := groupRe.FindStringSubmatch(profile.FileName)
+				if len(m) > 1 {
+					bucketName = m[1]
+				}
+				buckets[bucketName] = buckets[bucketName].add(block)
+			}
 		}
 		traceFn(fmt.Sprintf("Added %s", profile.FileName))
 	}
 
-	pct := "0.00%"
-	if totStmt > 0 {
-		pct = fmt.Sprintf("%.2f", float64(totCount)/float64(totStmt)*100)
+	if len(buckets) > 0 {
+		sortedBuckets := make([]string, 0, len(buckets))
+		for name, b := range buckets {
+			sortedBuckets = append(sortedBuckets, fmt.Sprintf("%s: %s", name, b.coverPct()))
+		}
+		sort.Strings(sortedBuckets)
+		for _, b := range sortedBuckets {
+			fmt.Println(b)
+		}
 	}
 
-	fmt.Printf("total: (statements) %s%%\n", pct)
+	fmt.Printf("total: %s\n", total.coverPct())
 
 	return nil
 }
